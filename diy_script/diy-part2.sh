0708cfc10bf8fb2f9e4b493b6e0c334e43312ed9
Dty <96579381+Dtyyyyyy@users.noreply.github.com>
2023-01-09 23:49:30 +0800

    Update diy-part2.sh

--- diy_script/diy-part2.sh
+++ diy_script/diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.88/g' package/base-files/files/bin/config_generate
 
 # v2ray_server
 # sed -i s/"cp -a"/"ln -s"/g feeds/luci/applications/luci-app-v2ray-server/luasrc/model/cbi/v2ray_server/api/app.lua
